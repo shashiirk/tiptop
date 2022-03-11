ba126f66b5ed6c9ee4aed1ad96148d1b6eb30564
Shashikanth Reddy <kolanshashii@gmail.com>
2022-03-11 21:17:37 +0530

    added user menu

---
+++ components/Menu.js
@@ -0,0 +1,120 @@
+import styled from 'styled-components';
+import { useSelector } from 'react-redux';
+import OutsideClickHandler from 'react-outside-click-handler';
+import { useRouter } from 'next/router';
+// import { signOut } from 'firebase/auth';
+
+const Div = styled.div`
+  border: 1px #eee solid;
+  background-color: white;
+  padding: 6px 0;
+  border-radius: 6px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.05);
+  position: absolute;
+  top: 100%;
+  right: 16px;
+  z-index: 10;
+  font-size: 14px;
+  width: 240px;
+
+  .special {
+    padding: 8px 58px 8px 16px;
+
+    p:first-child {
+      font-weight: 600;
+    }
+
+    p:nth-child(2) {
+      margin-top: 4px;
+    }
+
+    .sign {
+      display: inline-block;
+      border: 1px #4a00e0 solid;
+      border-radius: 6px;
+      color: #4a00e0;
+      padding: 4px 12px;
+      margin: 10px 0 6px 0;
+      cursor: pointer;
+    }
+  }
+
+  .divider {
+    border-bottom: 1px #eee solid;
+    margin: 8px 0 6px 0;
+  }
+
+  .item {
+    padding: 8px 16px;
+    cursor: pointer;
+
+    &:active {
+      background-color: #f4f4f4;
+    }
+  }
+`;
+
+const Menu = ({ onClose, onSignOut }) => {
+  const router = useRouter();
+  const user = useSelector((state) => state.auth.user);
+
+  const signInHandler = () => {
+    router.push('/signin');
+    onClose();
+  };
+
+  const collectionsHandler = () => {
+    router.push('/collections');
+    onClose();
+  };
+
+  const wishlistHandler = () => {
+    router.push('/wishlist');
+    onClose();
+  };
+
+  const cartHandler = () => {
+    router.push('/cart');
+    onClose();
+  };
+
+  return (
+    <Div>
+      <OutsideClickHandler onOutsideClick={onClose}>
+        <div className="special">
+          {user ? (
+            <>
+              <p>Hello</p>
+              <p>{user.email}</p>
+            </>
+          ) : (
+            <>
+              <p>Welcome</p>
+              <p>To access wishlist or cart</p>
+              <div className="sign" onClick={signInHandler}>
+                Sign In
+              </div>
+            </>
+          )}
+        </div>
+        <div className="divider"></div>
+        <div className="item" onClick={collectionsHandler}>
+          Collections
+        </div>
+        <div className="item" onClick={wishlistHandler}>
+          Wishlist
+        </div>
+        <div className="item" onClick={cartHandler}>
+          Cart
+        </div>
+        {user && (
+          <div className="item" onClick={onSignOut}>
+            Sign Out
+          </div>
+        )}
+      </OutsideClickHandler>
+    </Div>
+  );
+};
+
+export default Menu;
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,17 +1,23 @@
-import Link from 'next/link';
+import { useState } from 'react';
 import styled from 'styled-components';
+import { signOut } from 'firebase/auth';
 
 import { LogoIcon, WishlistIcon, CartIcon, UserIcon } from '../assets/icons';
 import BetterLink from './BetterLink';
+import Menu from './Menu';
+import { auth } from '../services/firebase-config';
 
 const Div = styled.div`
   /* border-bottom: 1px #374e62 solid; */
   display: flex;
   align-items: stretch;
   justify-content: space-between;
-  padding: 16px;
+  padding: 0 16px;
+  position: relative;
 
   .title {
+    /* border: 1px #374e62 solid; */
+    margin: 16px 0;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -36,43 +42,89 @@ const Div = styled.div`
     }
   }
 
-  .nav-items {
-    /* border: 1px green solid; */
-    list-style-type: none;
-    color: inherit;
+  .box {
+    /* border: 1px red solid; */
     display: flex;
-    align-items: center;
+    align-items: stretch;
+    /* position: relative; */
 
-    .nav-item {
-      /* border: 1px blue solid; */
-      color: #999;
+    .nav-items {
+      /* border: 1px green solid; */
+      list-style-type: none;
+      color: inherit;
       display: flex;
       align-items: center;
-      padding: 8px;
-      margin-right: 8px;
 
-      a {
-        text-decoration: none;
-        color: inherit;
+      .nav-item {
+        /* border: 1px blue solid; */
+        color: #999;
+        display: flex;
+        align-items: center;
+        padding: 8px;
 
-        @media (hover: hover) {
-          transition: color 240ms;
+        &:first-child {
+          margin-right: 8px;
+        }
 
-          &:hover {
-            color: #4a00e0;
+        a {
+          text-decoration: none;
+          color: inherit;
+
+          @media (hover: hover) {
+            transition: color 240ms;
+
+            &:hover {
+              color: #4a00e0;
+            }
           }
         }
       }
+    }
 
-      &:last-child {
-        height: 100%;
-        margin-right: 0;
+    button {
+      background-color: white;
+      border: none;
+      border-top: 3px transparent solid;
+      border-bottom: 3px transparent solid;
+      color: #999;
+      padding: 16px;
+
+      &.active {
+        border-bottom-color: #4a00e0;
       }
     }
   }
 `;
 
 const NavBar = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const toggleMenuHandler = () => {
+    if (isMenuVisible) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  };
+
+  const openMenu = () => {
+    setIsMenuVisible(true);
+  };
+
+  const closeMenu = () => {
+    setIsMenuVisible(false);
+  };
+
+  const signOutHandler = () => {
+    signOut(auth)
+      .then(() => {
+        closeMenu();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <Div>
       <h1 className="title">
@@ -81,21 +133,29 @@ const NavBar = () => {
           <p>tiptop</p>
         </BetterLink>
       </h1>
-      <ul className="nav-items">
-        <li className="nav-item">
-          <BetterLink href="/wishlist">
-            <WishlistIcon />
-          </BetterLink>
-        </li>
-        <li className="nav-item">
-          <BetterLink href="/cart">
-            <CartIcon />
-          </BetterLink>
-        </li>
-        <li className="nav-item">
+      <div className="box">
+        <button
+          className={`${isMenuVisible ? 'active' : ''}`}
+          onClick={toggleMenuHandler}
+        >
           <UserIcon />
-        </li>
-      </ul>
+        </button>
+        {isMenuVisible && (
+          <Menu onClose={closeMenu} onSignOut={signOutHandler} />
+        )}
+        <ul className="nav-items">
+          <li className="nav-item">
+            <BetterLink href="/wishlist">
+              <WishlistIcon />
+            </BetterLink>
+          </li>
+          <li className="nav-item">
+            <BetterLink href="/cart">
+              <CartIcon />
+            </BetterLink>
+          </li>
+        </ul>
+      </div>
     </Div>
   );
 };
