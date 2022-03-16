809c8dea71ff252e57a84bbeeb56a4688df6a5ce
Shashikanth Reddy <kolanshashii@gmail.com>
2022-03-16 12:34:55 +0530

    shuffle items

--- utils/getItems.js
+++ utils/getItems.js
@@ -1,7 +1,28 @@
 import data from '../pages/api/data.json';
 
+// Shuffle the items
+const shuffle = (array) => {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
+
 const getItems = () => {
-  return data.clothes;
+  return shuffle(data.clothes);
 };
 
 export default getItems;
