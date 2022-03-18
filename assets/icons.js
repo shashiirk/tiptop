dace0be205a40677320686c3a558ef6a1773b786
Shashikanth Reddy <kolanshashii@gmail.com>
2022-03-18 13:33:21 +0530

    deleted icon

--- assets/icons.js
+++ assets/icons.js
@@ -184,23 +184,3 @@ export const FilterIcon = () => (
     <line x1="18" y1="9" x2="18" y2="20" />
   </svg>
 );
-
-export const ArrowRightIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="icon icon-tabler icon-tabler-arrow-right"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    stroke="currentColor"
-    fill="none"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <line x1="5" y1="12" x2="19" y2="12" />
-    <line x1="13" y1="18" x2="19" y2="12" />
-    <line x1="13" y1="6" x2="19" y2="12" />
-  </svg>
-);
